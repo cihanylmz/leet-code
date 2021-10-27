37f2b19a088979d67b1d52d4122abb8dd365b327
cihanylmz <ylmz_cihan@outlook.com>
2021-10-27 21:46:40 +0300

    updated

---
+++ cmake-build-debug/CMakeFiles/leet_code.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leet_code.dir/main.cpp.obj"
+  "leet_code.exe"
+  "leet_code.exe.manifest"
+  "leet_code.pdb"
+  "libleet_code.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leet_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/leet_code.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for leet_code.
+# This may be replaced when dependencies are built.

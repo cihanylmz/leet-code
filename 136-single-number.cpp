9b4194696b599e3b26e98abd51710411a77b2c37
cihanylmz <ylmz_cihan@outlook.com>
2021-10-27 22:06:33 +0300

    updated

--- 136-single-number.cpp
+++ 136-single-number.cpp
@@ -1,10 +1,11 @@
 //LeetCode 136. Try to find the number which is not recurring in the list.
+#include <vector>
 
-int singleNumber(int A[], int n) {
+int singleNumber(std::vector<int>& nums) {
     int result = 0;
-    for (int i = 0; i<n; i++)
+    for (int i = 0; i<nums.size(); i++)
     {
-        result ^=A[i]; //Used XOR. If list is [2,2,1,1,3], this line corresponds to:
+        result ^=nums[i]; //Used XOR. If list is [2,2,1,1,3], this line corresponds to:
     }                  //0 ^ 2 ^ 2 ^ 1 ^ 1 ^ 3. x^x = 0, so at the and only : 0 ^ 3 remains. Which is 3
     return result;
 }
